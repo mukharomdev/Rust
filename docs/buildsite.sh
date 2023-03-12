4bbf6ee165b2c428cc5a858d00e453d7aeb3ab50
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2023-03-12 07:55:46 +0700

    update builsite.sh

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -2,12 +2,12 @@
 set -x
 
 apt-get update
-apt-get -y install git rsync python3-sphinx sphinx-rtd-theme make
+apt-get -y install git rsync python3-sphinx sphinx-rtd-theme automake
 
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
-cp -rf docsourc/* docs
-cd docs
+cp -rf docsourc/* .
+
 ##############
 # BUILD DOCS #
 ##############
