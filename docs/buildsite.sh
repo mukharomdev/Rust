64a462e5f92a78793e24a00a90240715ba9a8ba3
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2023-03-12 15:44:46 +0700

    update buildsite.sh

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -2,7 +2,7 @@
 set -x
 
 apt-get update
-apt-get -y install git rsync python3-sphinx sphinx-rtd-theme make
+apt-get -y install git rsync python3-sphinx 
 
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
