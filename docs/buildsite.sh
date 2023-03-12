71722e9a46402277d96ce692d9acf3f35d50ced3
mukharomdev <mukharomdev@gmail.com>
2023-03-12 14:29:35 +0700

    docs:edit docs

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -7,15 +7,15 @@ apt-get -y install git rsync python3-sphinx sphinx-rtd-theme
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
-cd docs
+
 ##############
 # BUILD DOCS #
 ##############
  
 # Python Sphinx, configured with source/conf.py
 # See https://www.sphinx-doc.org/
-make clean
-make html
+make -C docs clean
+make -C docs github
 
 #######################
 # Update GitHub Pages #
