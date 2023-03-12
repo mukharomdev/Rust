7b2684d19fc7fb35da6428bec240c62814edee1d
mukharomdev <mukharomdev@gmail.com>
2023-03-12 14:11:04 +0700

    docs:edit docs

--- docsourc/buildsite.sh
+++ docsourc/buildsite.sh
@@ -6,8 +6,7 @@ apt-get -y install git rsync python3-sphinx sphinx-rtd-theme
 
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
-
-cd docs
+ 
 ##############
 # BUILD DOCS #
 ##############
